39e87be73281156708240187bf14f51c64909ee1
Stephanie Riley <stephanie.riley-3@manchester.ac.uk>
2024-11-15 15:07:56 +0000

    add option to store cards in a list

--- R/generateCards.R
+++ R/generateCards.R
@@ -1,18 +1,36 @@
 ###### Function to creates cards for the magic trick
-generateCards<- function(ncards){
+generateCards<- function(ncards, altogether = F){
+  #altogether:  this argument is used for the purpose of presenting the cards
+  #             as separate tables within the app, or as one data frame for
+  #             export to csv
+  
   maxvalue<- 2^ncards - 1
   integers<- 1:maxvalue
   
   binarymatrix<- t(sapply(integers, int2bin, bits = 6, reverse = T))
   colnames(binarymatrix)<- 2^(1:ncol(binarymatrix))
   
-  cards<- data.frame()
-  for (i in 1:ncards) {
-    cards<- data.frame(rbind(cards,
-                             matrix(which(binarymatrix[,i] == 1), nrow = 2, byrow = T),
-                             #Include 2 rows of NA to separate the cards
-                             NA,
-                             NA))
+  if(altogether){
+    cards<- data.frame()
+    for (i in 1:ncards) {
+      #Want to print the card number in the row above the card, but don't want
+      #it to be repeated in each column
+      cardname<- c(paste0("Card ", i),
+                   rep("", length(which(binarymatrix[,i] == 1)) / 2))
+      cards<- data.frame(rbind(cards,
+                               cardname,
+                               #Extract only the numbers that have 1 in column i
+                               matrix(which(binarymatrix[,i] == 1), nrow = 2, byrow = T),
+                               #Include a row of NA to separate the cards
+                               ""))
+    }
+  }
+  
+  if(!altogether){
+    cards<- list()
+    for (i in 1:ncards) {
+      cards[[i]]<- matrix(which(binarymatrix[,i] == 1), nrow = 2, byrow = T)
+    }
   }
   
   return(cards)
